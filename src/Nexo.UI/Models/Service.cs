92e5d7f8afe15add2b8a4ffb641731caa9c4dbd0
Humberto Guedes <franciscohguedes@hotmail.com>
2024-05-21 07:24:45 -0300

    add: services section

---
+++ src/Nexo.UI/Models/Service.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics;
+
+namespace Nexo.UI.Models;
+
+[DebuggerDisplay("Title = {Title}, Icon = {Icon}, Body = {Body}")]
+public class Service
+{
+    public string? Title { get; set; }
+    public string? Icon { get; set; }
+    public string? Body { get; set; }
+}
--- src/Nexo.UI/Pages/Home.razor.cs
+++ src/Nexo.UI/Pages/Home.razor.cs
@@ -1,6 +1,7 @@
 using System.Net.Http.Json;
 using System.Text.Json;
 
+using Nexo.UI.Models;
 using Nexo.UI.Models.Summary;
 
 namespace Nexo.UI.Pages;
@@ -8,12 +9,14 @@ namespace Nexo.UI.Pages;
 public partial class Home
 {
     private Summary[]? _summary;
+    private Service[]? _services;
 
     protected override async Task OnInitializedAsync()
     {
         try
         {
             _summary = await Http.GetFromJsonAsync<Summary[]>("request/summaries.json");
+            _services = await Http.GetFromJsonAsync<Service[]>("request/services.json");
         }
         catch (HttpRequestException httpEx)
         {
