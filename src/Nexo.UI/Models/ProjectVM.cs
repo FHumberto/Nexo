707ed665a93c1bab1bc847be8d0b4f7d51c1fc1c
Humberto Guedes <franciscohguedes@hotmail.com>
2024-06-01 23:50:01 -0300

    add: estrutura da página de projetos

---
+++ src/Nexo.UI/Models/ProjectVM.cs
@@ -0,0 +1,14 @@
+﻿using Nexo.UI.Models.Summary;
+
+namespace Nexo.UI.Models;
+
+public class ProjectVM
+{
+    public string? Type { get; set; }
+    public string? Cover { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public List<Tool>? Tools { get; set; }
+    public string? Source { get; set; }
+    public string? Demo { get; set; }
+}
