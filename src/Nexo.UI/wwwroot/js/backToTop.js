6c8c4633fe66c0025993f6303a370e48aedd9ff4
Humberto Guedes <franciscohguedes@hotmail.com>
2024-06-04 13:41:09 -0300

    add: botão voltar para o topo

---
+++ src/Nexo.UI/wwwroot/js/backToTop.js
@@ -0,0 +1,14 @@
+﻿window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    let backToTopButton = document.getElementById("backToTopButton");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        backToTopButton.style.display = "block";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+}
+
+window.scrollToTopSmooth = function () {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
